fe1db12a62a4817668eaf8117b53b78574bc2d52
paulogolima <paulogolimacontato@gmail.com>
2026-05-01 00:58:08 -0300

    Criar controlador para processar requisições HTTP de usuários

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,37 +1,44 @@
+//Importação internas 
 import { UserService } from '../services/userService.js'
 
 class UserController {
 
+    // Controlador para buscar todos os usuários
     static async findAll(req,res) {
         UserService.findAll()
         .then(objs => res.json(objs))
         .catch(err => res.status(400).json({err: err.message}))
     }
-
+    
+    // Controlador para buscar um usuário pelo ID
     static async findByPk(req,res) {
         UserService.findByPk(req)
         .then(obj => res.json(obj))
         .catch(err => res.status(400).json({err: err.message}))
     }
 
+    // Controlador para criar um novo usuário
     static async create(req,res) {
         UserService.create(req)
         .then(obj => res.json(obj))
         .catch(err => res.status(400).json({err: err.message}))
     }
 
+    // Controlador para fazer login e retornar token JWT
     static async login(req,res) {
         UserService.login(req)
         .then(user => res.json(user))
         .catch(err => res.status(400).json({err: err.message}))
     }
 
+    // Controlador para atualizar dados de um usuário
     static async update(req,res) {
         UserService.update(req)
         .then(obj => res.json(obj))
         .catch(err => res.status(400).json({err: err.message}))
     }
 
+    // Controlador para deletar um usuário
     static async delete(req,res) {
         UserService.delete(req)
         .then(obj => res.json(obj))
