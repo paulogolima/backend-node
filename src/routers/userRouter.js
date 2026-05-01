0c08ab3012c96324a9f686620ff9a68e3187e0f7
paulogolima <paulogolimacontato@gmail.com>
2026-05-01 00:58:16 -0300

    Definir rotas da API com proteção de autenticação e autorização

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,14 +1,20 @@
+// Importações externas
 import express from "express"
 
+// Importações internas
 import { UserController } from "../controllers/userController.js"
+import { authMiddleware, adminMiddleware } from "../middlewares/auth.js"
 
+// Cria uma instância de router do Express
 const router = express.Router()
 
-// Adicionar às rotas a autenticação de token e admin
+// Rotas sem autenticação
 router.post('/register/user', UserController.create)
 router.post('/login/user', UserController.login)
-router.put('/user/:id',UserController.update)
-router.delete('/user/:id', UserController.delete)
-router.get('/users/listar', UserController.findAll)
+
+// Rotas com autenticação
+router.put('/user/:id',authMiddleware, UserController.update)
+router.delete('/user/:id', authMiddleware, UserController.delete)
+router.get('/users/listar', authMiddleware, adminMiddleware, UserController.findAll)
 
 export default router
