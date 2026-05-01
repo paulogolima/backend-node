f6de4f3b65a38ed5277fe9d9c527c2cf58d64d36
paulogolima <paulogolimacontato@gmail.com>
2026-04-30 22:09:16 -0300

    Formatar userRouter.js

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,10 @@
 import express from "express"
+
 import { UserController } from "../controllers/userController.js"
 
 const router = express.Router()
 
 // Adicionar às rotas a autenticação de token e admin
-
 router.post('/register/user', UserController.create)
 router.post('/login/user', UserController.login)
 router.put('/user/:id',UserController.update)
