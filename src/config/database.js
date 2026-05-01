d903a8b5fe2344b10cb750a20a30daa6ed024a51
paulogolima <paulogolimacontato@gmail.com>
2026-05-01 00:57:55 -0300

    Configurar conexão Sequelize com MySQL e variáveis de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,29 @@
+// Importações externas
+import "dotenv/config"
 import Sequelize from "sequelize"
 
+// Importações internas 
 import { User } from "../models/Users.js"
 
-const connection = new Sequelize({
-    dialect: 'sqlite',
-    storage: 'database.sqlite'
-})
+// Cria conexão Sequelize com as credenciais do banco de dados
+// As credenciais são obtidas das variáveis de ambiente (.env)
+const connection = new Sequelize(
+    process.env.NAME_DB,
+    process.env.USER_DB,   
+    process.env.PASS_DB
+,
+{
+    host: process.env.HOST_DB,
+    dialect: 'mysql'
+}
+)
 
-// Iniciar os models (Obrigatório)
+// Inicializa o modelo User com a conexão Sequelize (Obrigatório)
 User.init(connection)
 
-// Iniciar as associações (Se tiver relacionamentos)
-// Ex:
+// Inicializa as associações entre modelos (Se tiver relacionamentos)
+// Comentado pois este projeto não utiliza relacionamentos
 // User.associate(connection.models)
 
+// Exporta a conexão para ser utilizada em outros arquivos
 export default connection
