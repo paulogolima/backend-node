8ca33edef3af431c5f6072de9a38a9bf3c10a2b9
paulogolima <paulogolimacontato@gmail.com>
2026-05-04 18:31:21 -0300

    src/server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
-// Impoetações externas
+// Importações externas
 import express from 'express'
 
-// Importações internas
+// Importações internas - Configuração e Rotas
 import connection from './config/database.js'
 import {User} from './models/Users.js'
-import router from './routers/userRouter.js'
+import { userRoutes } from './routers/userRouter.js'
+import { pedidoRoutes } from './routers/pedidoRouter.js'
+import { itemRoutes } from './routers/itemRouter.js'
 
 // Cria instância do Express
 const app = express()
@@ -12,11 +14,14 @@ const app = express()
 // Obtém a porta do servidor das variáveis de ambiente
 const PORT = process.env.SERVER_PORT
 
-// Middleware para parsear JSON nas requisições
+// Middleware para parsear requisições JSON
 app.use(express.json())
 
 // Define as rotas da API no prefixo /api
-app.use('/api', router)
+// Todas as rotas de usuários, pedidos e itens passam por aqui
+app.use('/api', userRoutes)
+app.use('/api', pedidoRoutes)
+app.use('/api', itemRoutes)
 
 // Sincroniza os modelos com o banco de dados e inicia o servidor
 connection.sync().then(() => {
