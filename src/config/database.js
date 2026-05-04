8d87ac3c5bb7b212346e47dc212809f1ecbdf34f
paulogolima <paulogolimacontato@gmail.com>
2026-05-04 18:27:17 -0300

    src/config/database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,16 @@
 import "dotenv/config"
 import Sequelize from "sequelize"
 
-// Importações internas 
+// Importações internas - Modelos
+// Importa todos os modelos da aplicação (Users, Pedidos, Itens)
 import { User } from "../models/Users.js"
+import { Itens } from "../models/Itens.js"
+import { Pedidos } from "../models/Pedidos.js"
+
 
 // Cria conexão Sequelize com as credenciais do banco de dados
 // As credenciais são obtidas das variáveis de ambiente (.env)
+// Conexão usa MySQL como dialect
 const connection = new Sequelize(
     process.env.NAME_DB,
     process.env.USER_DB,   
@@ -18,12 +23,17 @@ const connection = new Sequelize(
 }
 )
 
-// Inicializa o modelo User com a conexão Sequelize (Obrigatório)
+// Inicializa todos os modelos com a conexão Sequelize (Obrigatório)
+// Cada init() deve ser chamado antes de usar o modelo
 User.init(connection)
+Itens.init(connection)
+Pedidos.init(connection)
 
-// Inicializa as associações entre modelos (Se tiver relacionamentos)
-// Comentado pois este projeto não utiliza relacionamentos
-// User.associate(connection.models)
+// Inicializa as associações entre modelos
+User.associate(connection.models)
+Pedidos.associate(connection.models)
+Itens.associate(connection.models)
+
 
 // Exporta a conexão para ser utilizada em outros arquivos
 export default connection
